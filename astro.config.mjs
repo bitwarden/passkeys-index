f2c19d6d46b2e64ba27b36e3fefd2168287b68cc
Mark Youssef <myoussef@bitwarden.com>
2024-03-07 12:33:53 -0800

    Setup site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://passkeys-index.netlify.app",
+  site: "https://passkeyindex.io",
   integrations: [mdx(), tailwind()],
 });
