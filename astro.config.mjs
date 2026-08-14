e84aae93ed3217ab102b5086705d48581ea09686
Mark Youssef <141061617+mark-youssef-bitwarden@users.noreply.github.com>
2026-08-14 10:17:12 -0700

    Update to astro 7.1.1 (#156)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
-import tailwind from "@astrojs/tailwind";
-
 const SITE_URL = import.meta.env.SITE_URL;
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx()],
 });
